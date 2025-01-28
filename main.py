aca17e3363126c81ab6ccf2af3ab1c91abce65b5
qberozeph <artem4roblox@gmail.com>
2025-01-29 07:05:46 +0800

    added reqs

--- main.py
+++ main.py
@@ -2,52 +2,61 @@ from contextlib import asynccontextmanager
 
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi.middleware. cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import init_db
 import requests as rq
 
+
 class AddTask(BaseModel):
-  tg_id: int
-  title: str
+    tg_id: int
+    title: str
+
 
 class CompleteTask(BaseModel):
-  id: int
+    id: int
+
 
 @asynccontextmanager
 async def lifespan(app_: FastAPI):
-  await init_db()
-  print('Bot is ready')
-  yield
+    await init_db()
+    print('Bot is ready')
+    yield
 
-app = FastAPI(title="To DO App", lifespan=lifespan)
+
+app = FastAPI(title="To Do App", lifespan=lifespan)
+
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/api/tasks/{tg_id}")
 async def tasks(tg_id: int):
-  user = await rq.add_user(tg_id)
-  return await rq.get_tasks(user.id)
+    user = await rq.add_user(tg_id)
+    return await rq.get_tasks(user.id)
+
 
 @app.get("/api/main/{tg_id}")
 async def profile(tg_id: int):
-  user = await rq.add_user(tg_id)
-  completed_tasks_count = await rq.get_completed_tasks_count(user.id)
-  return {'completedTasks': completed_tasks_count}
+    user = await rq.add_user(tg_id)
+    completed_tasks_count = await rq.get_completed_tasks_count(user.id)
+    return {'completedTasks': completed_tasks_count}
+
 
 @app.post("/api/add")
 async def add_task(task: AddTask):
-  user = await rq.add_user(task.tg_id)
-  await rq.add_user(user.id, task.title)
-  return {'status': 'ok'}
+    user = await rq.add_user(task.tg_id)
+    await rq.add_task(user.id, task.title)
+    return {'status': 'ok'}
+
 
 @app.patch("/api/completed")
-async def completed_task(task: CompleteTask):
-  await rq.update_task(task.id)
-  return {'status': 'ok'}
+async def complete_task(task: CompleteTask):
+    await rq.update_task(task.id)
+    return {'status': 'ok'}
--- requests.py
+++ requests.py
@@ -3,52 +3,58 @@ from models import async_session, User, Task
 from pydantic import BaseModel, ConfigDict
 from typing import List
 
-class TaskSchema(BaseModel):
-  id: int
-  title: str
-  completed: bool
-  user: int
 
-  model_config = ConfigDict(from_attributes=True)
+class TaskSchema(BaseModel):
+    id: int
+    title: str
+    completed: bool
+    user: int
+    
+    model_config = ConfigDict(from_attributes=True)
+
 
 async def add_user(tg_id):
-  async with async_session() as session:
-    User = await session.scalar(select(User).where(User.tg_id == tg_id))
-    if User:
-      return User
-    
-    new_user = User(tg_id=tg_id)
-    session.add(new_user)
-    await session.commit()
-    await session.refresh(new_user)
-    return new_user
-  
-async def get_tasts(user_id):
-  async with async_session() as session:
-    tasks = await session.scalars(
-      select(Task).where(Task.user == user_id, Task.completed == False)
-    )
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        if user:
+            return user
+        
+        new_user = User(tg_id=tg_id)
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return new_user
 
-    serialized_tasks = [
-      TaskSchema.model_validate(t).model_dump() for t in tasks
-    ]
 
-    return serialized_tasks
-  
+async def get_tasks(user_id):
+    async with async_session() as session:
+        tasks = await session.scalars(
+            select(Task).where(Task.user == user_id, Task.completed == False)
+        )
+        
+        serialized_tasks = [
+            TaskSchema.model_validate(t).model_dump() for t in tasks
+        ]
+        
+        return serialized_tasks
+
+
 async def get_completed_tasks_count(user_id):
-  async with async_session() as session:
-    return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
-  
+    async with async_session() as session:
+        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
+
+
 async def add_task(user_id, title):
-  async with async_session() as session:
-    new_task = Task(
-      title=title,
-      user=user_id
-    )
-    session.add(new_task)
-    await session.commit()
+    async with async_session() as session:
+        new_task = Task(
+            title=title,
+            user=user_id
+        )
+        session.add(new_task)
+        await session.commit()
+
 
 async def update_task(task_id):
-  async with async_session() as session:
-    await session.execute(update(Task).where(task_id == task_id).value(completed=True))
-    await session.commit()
+    async with async_session() as session:
+        await session.execute(update(Task).where(Task.id == task_id).values(completed=True))
+        await session.commit()
